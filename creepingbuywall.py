53f74c6d32a0d7706f3cd409355938ebca5786da
ids10998 <45244328+ids10998@users.noreply.github.com>
2018-12-26 16:30:22 +0000

    Add files via upload

--- creepingbuywall.py
+++ creepingbuywall.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import ccxt
+import numpy as np
 
 
 bittrex = ccxt.bittrex({
@@ -10,11 +11,26 @@ markets = bittrex.load_markets()
 whichmarket = (bittrex.symbols)
 
 cnx = mysql.connector.connect(user='root', password='Mumina12!23',
-                              host='localhost', database='coinbuys',
-                              auth_plugin='mysql_native_password')
+							  host='localhost', database='coinbuys',
+							  auth_plugin='mysql_native_password')
 
 mycursor = cnx.cursor()
 
+coin_pairs_start = [] 
+
+#get a list of coins that have buy walls
+
+
+sql1 = """SELECT * FROM BUYWALLDATA3 """
+mycursor.execute(sql1)
+myresult = mycursor.fetchall()
+for row in myresult:
+	coin_pairs_start.append(row[1])
+
+
+coin_pairs = list(set(coin_pairs_start))
+
+#define a few things
 avg_coin_price = {}
 coin_pairs_ticker = []
 f=0
@@ -22,23 +38,26 @@ count_row = 0
 avg_price_start = 0
 avg_price = 0
 price = 0
-while f < len(whichmarket):
-	sql = """SELECT * FROM BUYWALLDATA3 WHERE COINPAIR = '%s' """ % (whichmarket[f])
+#avg_price_start =[]
+
+while f < len(coin_pairs):
+	sql = """SELECT * FROM BUYWALLDATA3 WHERE COINPAIR = '%s' """ % (coin_pairs[f])
 	mycursor.execute(sql)
 	myresult = mycursor.fetchall()
 	for row in myresult:
-		
-		price = row[2]
-	    total = row[3]
-	    coin_pair #finish this 
+		price=row[2]
+		total=row[3]
+		avg_price_start = avg_price_start + float(row[2])
+		count_row = count_row + 1
+		avg_price = avg_price_start / count_row
+		#avg_price_start.append(row[2])
+		#for c in avg_price_start:
+		#	print (avg_price_start)
 
-	    avg_price_start = avg_price_start + float(price)
-	    count_row = count_row + 1
-	    avg_price = avg_price_start / count_row
 	coin_pairs_ticker.append(coin_pairs_ticker)    
-	print(whichmarket[f])
-	print (avg_price)    
-	avg_coin_price[whichmarket[f]]=avg_price
+	print(coin_pairs[f])
+	print ('avergae price: ',avg_price)    
+	avg_coin_price[coin_pairs[f]]=avg_price
 
 	count_row = 0    
 	avg_price_start = 0
@@ -46,23 +65,17 @@ while f < len(whichmarket):
 
 print(avg_coin_price)
 
-def Remove(coin_pairs_ticker): 
-    final_list = [] 
-    for num in duplicate: 
-        if num not in final_list: 
-            final_list.append(num) 
-    return final_list 
+#coin_pairs_ticker = list(set(coin_pairs_ticker))
 
-Remove(coin_pairs_ticker)
 
 limit = '10%'
 f = 0
 coins_with_buy_walls = {}
 coins_with_creeping_buy_walls =[]
 
-while f < len(coin_pairs_ticker):
-	orderbook = ccxt.bittrex().fetch_order_book(coin_pairs_ticker[f], limit)
-	ticker_info = bittrex.fetch_ticker(coin_pairs_ticker[f]) # ticker for a random symbol
+while f < len(coin_pairs):
+	orderbook = ccxt.bittrex().fetch_order_book(coin_pairs[f], limit)
+	ticker_info = bittrex.fetch_ticker(coin_pairs[f])
 	last_price = ticker_info.get('last')
 	volume = ticker_info.get('quoteVolume')
 
@@ -99,13 +112,13 @@ while f < len(coin_pairs_ticker):
 		if sum_array_check > threshhold and volume_threshold < 0.037:
 
 			#check if theres creeping buy orders 
-			in_key_avg_price = avg_coin_price.get(coin_pairs_ticker[f])
+			in_key_avg_price = avg_coin_price.get(coin_pairs[f])
 			# the value 1.03 is 3% above the last buy order price 
 			if (var_element_check[0] * 1.03) > in_key_avg_price:
 
 				print("Creeping Buy order present")
-				print(coin_pairs_ticker[f])
-				coins_with_creeping_buy_walls.append(coin_pairs_ticker[f])
+				print(coin_pairs[f])
+				coins_with_creeping_buy_walls.append(coin_pairs[f])
 
 			
 
@@ -120,24 +133,30 @@ while f < len(coin_pairs_ticker):
 
 	
 	if above_average_count >= 1:
-		coins_with_buy_walls[coin_pairs_ticker[f]] = above_average_count
+		coins_with_buy_walls[coin_pairs[f]] = above_average_count
 
 
 	
-	print ("The coin is;",whichmarket[f])
+	print ("The coin is;",coin_pairs[f])
 	print ("There are",above_average_count,"buy walls")
 	print (total_bids,"is the sum of orders")
 	print ("The average bid is",average_bid)
 	f = f + 1
 
-def Remove(coins_with_creeping_buy_walls): 
-    final_list = [] 
-    for num in duplicate: 
-        if num not in final_list: 
-            final_list.append(num) 
-    return final_list 
-      
+coins_with_creeping_buy_walls = list(set(coins_with_creeping_buy_walls))
+
+print (coins_with_creeping_buy_walls)	
 
 
-print (coins_with_buy_walls)
-print (Remove(coins_with_creeping_buy_walls))		
+
+
+
+
+
+
+
+
+
+
+
+
